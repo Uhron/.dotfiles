9e54ffaa9776f4b925659a1f6d7f68d587d71175
Álex <avasfel@gmail.com>
2024-05-13 20:01:03 +0200

    Update ubuntu_install.sh
    my way

--- nvim/ubuntu_install.sh
+++ nvim/ubuntu_install.sh
@@ -3,10 +3,11 @@
 #Inspired on https://github.com/prlz77/nvim
 
 # Dependencies
-sudo apt-get -y install python3-dev python3-pip python3-setuptools curl xclip silversearcher-ag fd-find
-sudo apt-get -y install build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev tk-dev
+sudo apt-get -y install curl xclip silversearcher-ag fd-find
+sudo apt-get -y install build-essential
+# sudo apt-get -y lib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev tk-dev
 
-ln -s $(which fdfind) ~/.local/bin/fd
+# ln -s $(which fdfind) ~/.local/bin/fd
 
 # install neovim
 sudo apt-get -y install software-properties-common
@@ -25,5 +26,5 @@ sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 sudo update-alternatives --auto editor
 
 # Common configuration
-#./common_install.sh
+./common_install.sh
 
